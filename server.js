891561ea6798d749d2cf1c49e04e3801af6aba4d
sergey <s.arkhipov@comradeweb.com>
2024-09-05 10:47:35 +0300

    test_fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import {MongoClient, ObjectId} from 'mongodb';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
+/**/
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocketServer({server});
